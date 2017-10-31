6fc089775b74edffd792f94721e48585e3e475a8
Lia Afework <lia.afework@yahoo.com>
2017-10-31 15:53:48 +0100

    Added variables, constructor and get methods.

--- src/kalle/Activities.java
+++ src/kalle/Activities.java
@@ -1,5 +1,36 @@
 package kalle;
 
-public class Activities {
+import java.time.LocalDate;
 
+public class Activities {
+	
+	private LocalDate date;
+	private String description;
+	private boolean expired;
+	
+	
+	public Activities(LocalDate date, String description) {
+		super();
+		this.date = date;
+		this.description = description;
+		this.expired = false;
+	}
+
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public boolean isExpired() {
+		return expired;
+	}
+
+	
+	
 }

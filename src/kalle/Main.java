b6a85fb6035ea5f0d07669db72d174bddefed1c7
Tommy Steger <tommysteger@gmail.com>
2017-10-31 15:58:35 +0100

    Menyn part 1

--- src/kalle/Main.java
+++ src/kalle/Main.java
@@ -1,11 +1,63 @@
 package kalle;
 
+import java.util.Scanner;
+import java.util.function.ToIntFunction;
+
+
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String message = "Hejsan";
-		String m2 ="Hello2";
-	}
+	public static Scanner sc = new Scanner(System.in);
 
+	public static void main(String[] args) {
+	
+		boolean flag = true;
+		while(flag){
+
+			meny();
+			
+			int a = inputNrGetNr();
+			switch (a) {
+			case 1:
+				System.out.println("Case "+a); // Tempory
+				break;
+			case 2:
+				System.out.println("Case "+a); // Tempory
+				break;
+			case 3:
+				System.out.println("Case "+a); // Tempory
+				break;
+			default:
+				flag=false;
+				break;
+			}
+			System.out.println("Exit2 "+a);
+
+			
+		}
+
+		sc.close();
+	}
+		
+	public static int inputNrGetNr() {
+		boolean flag = true;
+		int number;
+		try {
+			number = sc.nextInt();
+			while(flag) {
+
+				if(number !=0) {
+					//retNr=number;
+					flag=false;
+				}
+			}
+			return number;
+		} catch (ArithmeticException e) {
+			System.out.println("Enter a number");
+			return 0;
+		}
+	}
+	public static void meny() {
+		System.out.println("Press 1 for Item1\nPress 2 for  Item2\nPress 3 for  Item3");
+	}
 }

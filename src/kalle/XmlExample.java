9ac85df94b4c70debb472d319a912eb173e78c47
Tommy Steger <tommysteger@gmail.com>
2017-11-02 11:18:04 +0100

    XML SAVE AND LOAD , and example file

---
+++ src/kalle/XmlExample.java
@@ -0,0 +1,34 @@
+                
+                
+/*
+
+				case "save": {
+                    // Using XmlIO to save an object to file, errors are unexpected (write protected files)
+                    try {
+                        XMLIO.saveObject("todolist.xml", todoList);
+                        break;
+                    } catch (IOException ex) {
+                        Logger.getLogger(Todo.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                case "load": {
+                    // Loading with XmlIO, in this case the file might be missing.
+                    try {
+                        todolist = XMLIO.loadObject("todolist.xml", TodoList.class);
+                    } catch (IOException ex) {
+                        System.out.println("Could not load todolist.xml");
+                    }
+                    break;
+					
+					//Where TodoList.class is a seperate class containing the collection to be saved to xml
+			        //Add default constructor and use public fields in TodoList.class     
+                    //data is the object to be saved to xml
+					//The xml file should be placed in the root of the project
+
+
+*/
+
+					
+					
+					
+					
